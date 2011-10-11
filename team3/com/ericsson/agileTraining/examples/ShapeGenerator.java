94d726a023558aa94c028f39f4445f079c9cf244
euyacdf <anusha.k@ericsson.com>
2011-10-11 08:32:07 +0200

    Changes for circle

--- team3/com/ericsson/agileTraining/examples/ShapeGenerator.java
+++ team3/com/ericsson/agileTraining/examples/ShapeGenerator.java
@@ -5,9 +5,17 @@ public class ShapeGenerator
 
     public static void main(String[] args)
     {
-        Rectangle theR1 = new Rectangle(10.4f, 15.3f);
+        Rectangle theR1 = new Rectangle();
+        theR1.setLength(10.5f);
+        theR1.setBreadth(5.8f);
         theR1.printDimensions();
         theR1.printArea();
+        
+        System.out.println("\n");
+        Circle theCircle1 = new Circle();
+        theCircle1.setRadius(5);
+        theCircle1.printDimensions();
+        theCircle1.printArea();
     }
 }
 
