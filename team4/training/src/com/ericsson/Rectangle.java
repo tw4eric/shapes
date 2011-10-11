bd81add3eef75b17584e387ac82e041be12271ef
epalaar <palaniappan.arunachalam@ericsson.com>
2011-10-11 08:19:02 +0200

    changes

--- team4/training/src/com/ericsson/Rectangle.java
+++ team4/training/src/com/ericsson/Rectangle.java
@@ -1,6 +1,6 @@
 package com.ericsson;
 
-public class Rectangle {
+public class Rectangle implements Shape {
 
 	private double height;
 	private double width;
--- team4/training/src/com/ericsson/RenderShape.java
+++ team4/training/src/com/ericsson/RenderShape.java
@@ -3,15 +3,15 @@ package com.ericsson;
 public class RenderShape {
 
 	public static void main(String[] args) {
-		Rectangle rectangle1 = new Rectangle(10, 20);
-		Rectangle rectangle2 = new Rectangle(5.0, 10.5);
-		Rectangle rectangle3 = new Rectangle(3, 25.6);
+		Shape rectangle1 = new Rectangle(10, 20);
+		Shape rectangle2 = new Rectangle(5.0, 10.5);
+		Shape rectangle3 = new Rectangle(3, 25.6);
 
 		rectangle1.draw();
 		rectangle2.draw();
 		rectangle3.draw();
 		
-		new Rectangle(-3, 25.6);
+		//new Rectangle(-3, 25.6);
 	}
 
 }
