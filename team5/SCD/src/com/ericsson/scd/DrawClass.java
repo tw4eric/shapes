03cbef849b55e4939ca4f5990eaf703980fdd605
esensek <senthil.p.sekar@ericsson.com>
2011-10-11 12:03:06 +0530

    Updated Code

--- team5/SCD/src/com/ericsson/scd/DrawClass.java
+++ team5/SCD/src/com/ericsson/scd/DrawClass.java
@@ -1,6 +1,9 @@
 package com.ericsson.scd;
 
-class Rectangle{
+interface Shape{
+	public void draw();
+}
+class Rectangle implements Shape{
 	private int length;
 	private int breadth;
 	public Rectangle(int length, int breadth) {
@@ -9,22 +12,63 @@ this.length = length;
 this.breadth = breadth;
 	}
 	public void draw(){
-		System.out.println("------");
+		System.out.println("------Rectangle");
 		System.out.println("The Length is :"+this.length);
 		System.out.println("The Breadth is  :"+this.breadth);
 		System.out.println("The Area is :"+(this.length * this.breadth));
 		System.out.println();
 	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public int getBreadth() {
+		return breadth;
+	}
+	public void setBreadth(int breadth) {
+		this.breadth = breadth;
+	}
 }
 
+class Circle implements Shape{
+	private double radius;
+	
+	public Circle(double radius) {
+		// TODO Auto-generated constructor stub
+this.radius = radius;
+
+	}
+	public void draw(){
+		System.out.println("------Circle");
+		System.out.println("The Radius is :"+this.radius);
+	
+		System.out.println("The Area is :"+Math.pow(this.radius,2) * (22/7));
+		System.out.println();
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+}
 public class DrawClass {
 	public static void main(String[] args) {
-		for(int i=1;i<=20;i++){
+		for(int i=1;i<=5;i++){
 		int length = 10+i;
 		int breadth = 30-i;
-			Rectangle rect = new Rectangle(length, breadth);
+		if(i%2==0){	
+		Shape rect = new Rectangle(length, breadth);
 			rect.draw();
-		}	
+		}
+		else
+		{
+			Shape circle = new Circle(Math.random()*100);
+			circle.draw();	
+		}
+					}	
 	}
 	
 }
