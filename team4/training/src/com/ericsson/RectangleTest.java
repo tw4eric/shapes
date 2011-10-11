57c8ddf398ddc0ba814e6b93abd47426d02f0d58
epalaar <palaniappan.arunachalam@ericsson.com>
2011-10-11 07:59:33 +0200

    testcase for rectangle

--- team4/training/src/com/ericsson/RectangleTest.java
+++ team4/training/src/com/ericsson/RectangleTest.java
@@ -1,5 +1,28 @@
 package com.ericsson;
 
-public class RectangleTest {
+import junit.framework.TestCase;
 
+public class RectangleTest extends TestCase {
+
+	public RectangleTest(String name) {
+		super(name);
+	}
+
+	public void testRectanglePositive() {
+		try {
+			new Rectangle(10, 20);
+		} catch (Exception e) {
+			fail("Excepion");
+		}
+	}
+
+	public void testRectangleNegative() {
+		try {
+			new Rectangle(-10, 20);
+		} catch (Exception e) {
+			assertEquals(
+					"Height and width of a rectangle should be greater than zero.", 
+					e.getMessage());
+		}
+	}
 }
