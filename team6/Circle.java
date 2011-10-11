48c2bb41ed179a610bcfd14ce3f06230838e7bcb
esrarao <s.rajaram.mohan.rao@ericsson.com>
2011-10-11 12:21:16 +0530

    chg

---
+++ team6/Circle.java
@@ -0,0 +1,10 @@
+class Circle
+{
+	static void genCircle()
+	{
+	double radius= 1+Math.random()*10;
+	System.out.println("radius  "+radius);
+	System.out.println("area  "+ 3.14 * radius *radius);
+	}
+
+	}
