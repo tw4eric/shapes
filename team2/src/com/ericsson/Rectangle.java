d1ffa66706863bfdc664034eee9f2c9dacd0916a
esensek <senthil.p.sekar@ericsson.com>
2011-10-12 12:57:14 +0530

    Test For Rectangle

--- team2/src/com/ericsson/Rectangle.java
+++ team2/src/com/ericsson/Rectangle.java
@@ -5,27 +5,29 @@ public class Rectangle implements Shape
 
     private int width;
     private int height;
-    private int area;
+    
     
     public Rectangle(int width, int height)
     {
+    	if(width<0 || height<0)
+    		throw new IllegalArgumentException();
+    	
         this.width  = width;
         this.height = height;
     }
     
-    public void draw()
-    {
-        area  = (width * height);
-        System.out.println("Rectangle dimensions drawn.");
-        System.out.println("Width: " + width + ", Height: " + height);
-        System.out.println("Total area: "  + area);
-        System.out.println();
-    }
-
+   
     @Override
-    public int getArea()
+    public void render(Bitmap bitmap)
     {
-        return area;
+        
         
     }
+
+
+	@Override
+	public double computeArea() {
+		// TODO Auto-generated method stub
+		return width*height;
+	}
 }
--- team2/src/com/ericsson/Shape.java
+++ team2/src/com/ericsson/Shape.java
@@ -2,6 +2,6 @@ package com.ericsson;
 
 public interface Shape
 {
-    public void draw();
-    public int getArea();
+    public double computeArea();
+    public void render(Bitmap bitmap);
 }
